dc3e13e96b7b5f3b9509cb49b0f1efeb9ec9ffb0
Stanislav Ivanov <stasdat@gmail.com>
2017-11-26 19:25:24 +0200

    wasm is working

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+import loadWasm from './add.wasm';
+
 const container = document.querySelector('#app');
-container.innerHTML = 'hi';
+
+loadWasm().then(loaded => {
+    const { add } = loaded.instance.exports; 
+
+    container.innerHTML = `wasm: 2 + 3 = ` + add(2, 3);
+})
